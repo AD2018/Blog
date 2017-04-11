b39cab10c5b6581beafb176fb92fed95f9f503e1
Al`mir <darzemanov668@gmail.com>
2017-04-11 19:07:26 +0300

    add main page #index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'articles#index'
+
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
